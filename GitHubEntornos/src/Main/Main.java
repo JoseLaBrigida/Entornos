22b753b3aadf365e1312822a157b4397b40d5250
Jose <Jose@DESKTOP-5HASLMD>
2023-04-13 18:02:36 +0200

    comit de mi rama bailón

--- GitHubEntornos/src/Main/Main.java
+++ GitHubEntornos/src/Main/Main.java
@@ -1,11 +1,18 @@
 package Main;
 
 public class Main {
+	public static void funcionBailon() {
+		System.out.println("Esta es la rama de Bailon");
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("jamau");
+		
+		System.out.println("Bailón escribiendo en el metodo");
 	}
 
 }
