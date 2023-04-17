cbbf38e2630e28ec219d826706e0d4c6eec35fb1
Enri2909 <etorgom2909@g.educaand.es>
2023-04-17 19:35:49 +0200

    Agrego el metodo Henry

--- GitHubEntornos/src/Main/Main.java
+++ GitHubEntornos/src/Main/Main.java
@@ -8,11 +8,18 @@ public class Main {
 		System.out.println("Hola gente");
 		
 		despedida();
+		accionHenry();
 	}
 
 	private static void despedida() {
 		// TODO Auto-generated method stub
 		System.out.println("Adios, vayase con dios");
+	}
+	private static void accionHenry() {
+		System.out.println("2+2=5");
+		
+		
+		
 	}
 
 }
